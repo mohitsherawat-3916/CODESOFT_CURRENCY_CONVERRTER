4ad72dbf37e0e8b4b31c0980ccf8b819c7da5272
mohit sherawat <mohitsherawat3916@gmail.com>
2025-07-24 19:53:13 +0530

    update

--- currency_converter.java
+++ currency_converter.java
@@ -13,41 +13,41 @@ public class currency_converter {
         System.out.println("6 Indian Rupee to Nepalese Rupee");
 
         System.out.print("Choose a Number : ");
-        double number=sc.next.Double();
+        double number=sc.nextDouble();
 
         if(number==1){
             System.out.println("Enter US Dollar");
-            double n=sc.nextInt();
+            double n=sc.nextDouble();
             double rupee=n*85.7521;
             System.out.println(rupee + " Indian Rupee");
 
         }else if(number==2){
             System.out.println("Enter Indian Rupee");
-            double n=sc.nextInt();
+            double n=sc.nextDouble();
             double rupee=n*0.0117;
             System.out.println(rupee + " US Dollar");
 
         }else if(number==3){
             System.out.println("Enter Russian Ruble");
-            double n=sc.nextInt();
+            double n=sc.nextDouble();
             double rupee=n*1.097;
             System.out.println(rupee + " Indian Rupee");
 
         }else if(number==4){
             System.out.println("Enter Indian Rupee : ");
-            double n=sc.nextInt();
+            double n=sc.nextDouble();
             double rupee=n*0.9116;
             System.out.println(rupee + " Russian Ruble : ");
 
         }else if(number==5){
             System.out.println("Enter Nepalese Rupee : ");
-            double n=sc.nextInt();
+            double n=sc.nextDouble();
             double rupee=n*0.62;
             System.out.println(rupee + " Indian Rupee : ");
 
         }else if(number==6){
             System.out.println("Enter Indian Rupee : ");
-            double n=sc.nextInt();
+            double n=sc.nextDouble();
             double rupee=n*1.6;
             System.out.println(rupee + " Nepalese Rupee : ");
 
